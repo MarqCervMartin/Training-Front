12e36954a5691612f7688d4acea0d00e734549eb
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-22 12:56:51 -0500

    Diferentes tipos de botones

--- disenio-material/src/App.jsx
+++ disenio-material/src/App.jsx
@@ -1,10 +1,50 @@
 // IMPORTANDO UN BOTON
 import Button from '@material-ui/core/Button'
 
+
+
+
+
+//SEGUNDA OPCION PARA IMPORTAR
+//import {Button} from '@material-ui/core'
+
+
 function App() {
   return (
     <div>
-      <button>Este es un boton</button>
+      <Button color="default">
+        Color
+      </Button>
+
+      <Button variant="contained" color="secondary">
+        Containded
+      </Button>
+
+      <Button variant="contained" color="secondary" disableElevation>
+        Sin Sombra
+      </Button>
+
+      <Button variant="contained" color="secondary" href="https://google.com">
+        href
+      </Button>
+
+      <Button variant="contained" color="secondary" fullWidth>
+        fullwidth
+      </Button>
+
+      <Button variant="contained" color="secondary" size="small">
+        small
+      </Button>
+
+      <Button variant="contained" color="secondary" size="medium">
+        medium
+      </Button>
+
+      <Button variant="contained" color="secondary" size="large">
+        large 
+      </Button>
+
+
     </div>
   );
 }
