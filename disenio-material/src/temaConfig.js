7ccf3bc09e47c71d5df05026c50fe5e9f77b1630
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-22 19:27:41 -0500

    personalizar elementos

--- disenio-material/src/temaConfig.js
+++ disenio-material/src/temaConfig.js
@@ -1,8 +1,12 @@
 import {createMuiTheme} from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple'
 
 const theme = createMuiTheme({
-
-
+    palette: {
+        primary: {
+            main: purple[700]
+        }
+    }
 })
 
 export default theme;
