ef0b3ef437a9158ad4672e87ac3a64bc4747a48d
Francisco Javier Migguel Garcia <franck281099@gmail.com>
2021-07-30 23:38:20 -0500

    Login whith google complete

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default{
+  apiKey: "AIzaSyBfFw5HIf76FTPWvT1WZ7OaMDzh_CxYfvY",
+  authDomain: "chatfirebase-4da56.firebaseapp.com",
+  databaseURL: "https://chatfirebase-4da56-default-rtdb.firebaseio.com",
+  projectId: "chatfirebase-4da56",
+  storageBucket: "chatfirebase-4da56.appspot.com",
+  messagingSenderId: "566574066915",
+  appId: "1:566574066915:web:2c0bca320ee725338a29d6",
+  measurementId: "G-85X44ZRV4K"
+}
