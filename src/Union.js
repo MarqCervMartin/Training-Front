47ec0ad125736e8bad999d88d9682397a8b7fcd8
Francisco Javier Migguel Garcia <franck281099@gmail.com>
2021-08-06 23:39:21 -0500

    Union de ramas Fusion con Chat

--- src/Union.js
+++ src/Union.js
@@ -3,12 +3,14 @@ import {auth} from './firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import FirstScreen from './components/Login/FirstScreen'
 import ThirdScreen  from './components/Listo/ThirdScreen'
+import App from './App.js';
+
 
 
 const Union = () => {
     const [user] = useAuthState(auth);
     return (
-            user ? <ThirdScreen/> : <FirstScreen/>
+            user ? <App/> : <FirstScreen/>
         
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './App.jsx';
 
+
 ReactDOM.render(
+    <React.StrictMode>
+    <App />
     </React.StrictMode>,
     document.getElementById('root')
-);
-// If you want your app to work offline and load faster, you can change
+    );
+    // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
