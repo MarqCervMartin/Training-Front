655243bef85eafcc35283660bb840ec9b2fcf14d
Francisco Javier Migguel Garcia <franck281099@gmail.com>
2021-08-08 11:27:29 -0500

    final

--- src/componentes/Perfil.jsx
+++ src/componentes/Perfil.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import {auth} from '../firebase';
 
 const Perfil = () => {
+
     return (
-        <div className="App">{
-            auth.currentUser.photoURL 
-          }   
+        
+        <div className="App">{            
+            <img src={auth.currentUser.photoURL}/>
+            
+          } 
+          <h1> <br/> </h1>
+          <h2>User Name: </h2>
+          <h1>{auth.currentUser.displayName}</h1>
+          
+          <h2> <br/> </h2>
+          <h2>Email: </h2>
+          <h1>{auth.currentUser.email}</h1>
+
         </div> 
     )
 }
